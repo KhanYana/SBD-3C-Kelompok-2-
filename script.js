90f6de6d3742387eba2442fc1a4ee466d254a40b
KhanYana <khansadayana8@gmail.com>
2026-09-23 20:47:32 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+  const isOpen = navMenu.classList.toggle('is-open');
+  navToggle.setAttribute('aria-expanded', String(isOpen));
+});
+
+document.querySelectorAll('.nav-menu a').forEach((link) => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('is-open');
+    navToggle.setAttribute('aria-expanded', 'false');
+  });
+});
